7b67877e57e132bcb6e05e2f4388e76e1a15adcd
Julius <kujulius@hotmail.com>
2012-03-15 20:38:30 -0500

    First Commit

---
+++ TDEW-PC1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TDEWPc1::Application.config.secret_token = 'c2c99da7b7f0b657a84a3f15f9c1982f0eab5491f7562ed3a44c823beeda5b9a99362a6e4b6da05505c506913a596bb06dd6c959e1dbf7c843583ddb6880c46f'
